0a0acf6e9c4c03509d167d0bbd75640dda67fa33
Jürgen Mülbert <juergen.muelbert@gmail.com>
2020-03-29 17:05:33 +0200

    Update travis istall.sh for mac.

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,22 +3,6 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew outdated cmake || brew upgrade cmake
-
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
-    pyenv rehash
-    pyenv activate conan
-fi
-
 pip install conan --upgrade
 pip install conan_package_tools
 
